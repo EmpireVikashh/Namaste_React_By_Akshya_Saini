d65e0350e9de0b4deee6b06ab1c8306959ee6755
Vikash sharma <116377338+EmpireVikashh@users.noreply.github.com>
2023-08-25 21:39:28 +0530

    Add files via upload

---
+++ SwiggyClone/src/App.js
@@ -0,0 +1,18 @@
+// import './App.css';
+import Header from './MyPage/Header/Header.js'
+import Body from './MyPage/Body/Body.js'
+import Footer from './MyPage/Footer/Footer.js'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header/>
+      <Body/>
+      <Footer/>
+      <h1> My App </h1>
+    </div>
+  );
+}
+
+export default App;
