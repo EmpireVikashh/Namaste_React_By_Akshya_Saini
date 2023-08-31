57699deec02d82d5a50fa30cb1397b796e2459ed
Vikash <empirevikash@gmail.com>
2023-08-31 23:41:10 +0530

    login

--- SwiggyClone/src/MyPage/Login/Login.js
+++ SwiggyClone/src/MyPage/Login/Login.js
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom";
+// import {Link} from "react-router-dom";
 const Login = ()=>{
     function handleSubmit(event){
         event.preventDefault();
@@ -7,13 +7,19 @@ const Login = ()=>{
     return(
         <form id="Login-form" onSubmit={handleSubmit}>
         <h1>Before Login Please SignUp</h1>
-        <div className="Login-Container">
-            <label>Username or Email:</label>
-            <input type="text" id="" placeholder="Enter Username"/>
-            <label>Password:</label>
-            <input type="text" id="" placeholder="Enter Password"/>
-            <button type="submit">Login</button>
-            <Link to="./" >Forgot Password</Link>
+        <div id="Login-Container">
+
+            <label className="login-child user-email" >Username or Email:</label>
+            <input type="text" id="user" className="login-child" placeholder="Enter Username"/>
+
+            <label className="login-child pass">Password:</label>
+            <input type="text" id="password" className="login-child" placeholder="Enter Password"/>
+
+            <button type="submit" id="login" className="login-child">Login</button>
+
+            <label className="login-child">Forgot Password</label>
+            {/* <Link to="/">Forgot Password</Link> */}
+            
         </div>
         </form>
     )
