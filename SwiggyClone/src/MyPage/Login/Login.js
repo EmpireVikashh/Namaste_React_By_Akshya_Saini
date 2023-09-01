5e1252397fa211b8025c35b3f8297d07c829ca57
Vikash sharma <116377338+EmpireVikashh@users.noreply.github.com>
2023-09-01 11:30:06 +0000

    login

--- SwiggyClone/src/MyPage/Login/Login.js
+++ SwiggyClone/src/MyPage/Login/Login.js
@@ -1,34 +1,38 @@
 // import {Link} from "react-router-dom";
-const Login = ()=>{
-    function handleSubmit(event){
+const Login = () => {
+    function handleSubmit(event) {
         event.preventDefault();
     }
-    
+
     // const [inpValue, setInpValue] = useState("");
     // import { useState } from 'react';
     // <input type="text" value={inpValue} onChange={(e)=>{
     //   setInpValue(e.target.value);
-    
+
     // }}
     // />
 
-    return(
+    return (
         <form id="Login-form" onSubmit={handleSubmit}>
-        <h1>Before Login Please SignUp</h1>
-        <div id="Login-Container">
+            <h1>Before Login Please SignUp</h1>
+            <div id="Login-Container">
 
-            <label className="login-child user-email" >Username or Email:</label>
-            <input type="text" id="user" className="login-child" placeholder="Enter Username"/>
+                <div className="login-child">
+                    <label className="login-child user-email" >Username or Email:</label>
+                    <input type="text" id="user" placeholder="Enter Username" />
+                </div>
 
-            <label className="login-child pass">Password:</label>
-            <input type="text" id="password" className="login-child" placeholder="Enter Password"/>
+                <div className="login-child">
+                    <label className="login-child pass">Password:</label>
+                    <input type="text" id="password" placeholder="Enter Password" />
+                </div>
 
-            <button type="submit" id="login" className="login-child">Login</button>
+                <button type="submit" id="login">Login</button>
 
-            <label className="login-child">Forgot Password</label>
-            {/* <Link to="/">Forgot Password</Link> */}
-            
-        </div>
+                <label className="forgot-password" >Forgot Password</label>
+                {/* <Link to="/">Forgot Password</Link> */}
+
+            </div>
         </form>
     )
 }
