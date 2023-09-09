90bdc3a07681b4204e268099f5a1f48f5f94f681
Vikash sharma <116377338+EmpireVikashh@users.noreply.github.com>
2023-09-09 11:09:57 +0000

    signUp

--- SwiggyClone/src/MyPage/SignUp/SignUp.js
+++ SwiggyClone/src/MyPage/SignUp/SignUp.js
@@ -1,25 +1,55 @@
+import {useState} from "react";
 const SignUp = ()=>{
+
+    // set all input value from user
+    const [value, setValue] = useState({
+        name:"",
+        email:"",
+        mobile:"",
+        createPass:"",
+        confirmPass:""
+    })
+    // Authenticate mandatry feilds
+    // const [error, setError] = useState("");
+
+    // Destructure my useState value
+    const {name,email,mobile,createPass,confirmPass} = value;
+
+    const onchangeInputHandle = (e)=>{
+        // console.log(e.target.value);
+        setValue({...value,[e.target.name]:[e.target.value]})
+    }
+
+    const formSubmitHandle = (e)=>{
+        e.preventDefault();
+        console.log(name, email, mobile, createPass, confirmPass);
+        if(name === " " || email === " " || mobile === " " || createPass === " " || confirmPass === " "){
+            console.log("feild required")
+        }
+
+    }
+
     return(
-        <form action="" id="signUp-form">
+        <form action="" id="signUp-form" onSubmit={formSubmitHandle}>
             <div id="signUp-container">
                 
-                    <input type="text" placeholder="Name" name="" id="name"/>
+                    <input value={name} placeholder="Name" name="name" id="name" onChange={onchangeInputHandle}/>
             
-                    <input type="text" placeholder="Email" name="" id="email"/>
+                    <input value={email} placeholder="Email" name="email" id="email" onChange={onchangeInputHandle}/>
                 
-                    <input type="text" placeholder="Mobile No." name="" id="number"/>
+                    <input value={mobile} placeholder="Mobile No." name="mobile" id="number" onChange={onchangeInputHandle}/>
             
-                    <input type="text"placeholder="Create Password" name="" id="password"/>
+                    <input value={createPass} placeholder="Create Password" name="createPass" id="password" onChange={onchangeInputHandle}/>
                 
-                    <input type="text" placeholder="Confirm Password" name="" id="confirm-password"/>
+                    <input value={confirmPass} placeholder="Confirm Password" name="confirmPass" id="confirm-password" onChange={onchangeInputHandle}/>
                 
-                    <button className="SignUp-Button" >SignUp</button>
+                    <button className="SignUp-Button">SignUp</button>
 
                     <div className="already-account">
                          Already have an account? <span>Login</span>
                     </div>
 
-                    <div className="login-with-Google">Login with google</div>
+                    <div className="login-with-Google">Login with Google</div>
             </div>
         </form>
         
