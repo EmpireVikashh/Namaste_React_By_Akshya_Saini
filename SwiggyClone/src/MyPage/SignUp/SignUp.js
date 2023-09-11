72dbf8e9770f6efa9a9db06a3f99a27348419e31
Vikash sharma <116377338+EmpireVikashh@users.noreply.github.com>
2023-09-11 12:41:12 +0000

    signup

--- SwiggyClone/src/MyPage/SignUp/SignUp.js
+++ SwiggyClone/src/MyPage/SignUp/SignUp.js
@@ -1,71 +1,83 @@
-import {useState} from "react";
-const SignUp = ()=>{
+import { useState } from "react";
+const SignUp = () => {
 
     // set all input value from user
     const [user, setUser] = useState({
-        name:"",
-        email:"",
-        mobile:"",
-        createPass:"",
-        confirmPass:""
+        name: "",
+        email: "",
+        mobile: "",
+        createPass: "",
+        confirmPass: ""
     })
-    console.log(user);
+    // console.log(user);
+
     // throw error if user put wrong info 
-    const [error, setError] = useState("error");
+    const [error, setError] = useState("");
+
+
+
 
     // Destructure my useState value
-    const {name,email,mobile,createPass,confirmPass} = user;
-    console.log(name.length);
+    const { name, email, mobile, createPass, confirmPass } = user;
+    console.log(user);
+    
 
-    const onchangeInputHandle = (e)=>{
-        setUser({...user,[e.target.name]:e.target.value})
+    const onchangeInputHandle = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value })
 
         // if user enter space at starting
-        if(e.target.value === " " && e.target.value.length === 1){
-        
-            setUser({...user,[e.target.name]:e.target.value.trim()})//used trim() for ignore space at starting
+        if (e.target.value === " ") {
+            setUser({ ...user, [e.target.name]: e.target.value.trim() })//used trim() for ignore space at starting
         }
-        // console.log(e.target.value);
 
+        if (name.length < 3) {
+            setError("length is too short");
+            
+        }
+        else {
+            console.log(error, "2");
+            setError("");
+        }
+        console.log(error, "1");
 
     }
 
-    const formSubmitHandle = (e)=>{
+    const formSubmitHandle = (e) => {
         e.preventDefault();
         console.log(name, email, mobile, createPass, confirmPass);
-        if(name === "" || email === "" || mobile === "" || createPass === "" || confirmPass === "" ){
+        if (name === "" || email === "" || mobile === "" || createPass === "" || confirmPass === "") {
             console.log("feild required")
         }
     }
 
-    return(
+    return (
         <form action="" id="signUp-form" onSubmit={formSubmitHandle}>
             <div id="signUp-container">
-                
-                    <input value={name} placeholder="Name" name="name" id="name" onChange={onchangeInputHandle}/>
-            
-                    <input value={email} placeholder="Email" name="email" id="email" onChange={onchangeInputHandle}/>
-                
-                    <input value={mobile} placeholder="Mobile No." name="mobile" id="number" onChange={onchangeInputHandle}/>
-            
-                    <input value={createPass} placeholder="Create Password" name="createPass" id="password" onChange={onchangeInputHandle}/>
-                
-                    <input value={confirmPass} placeholder="Confirm Password" name="confirmPass" id="confirm-password" onChange={onchangeInputHandle}/>
 
-                    <div className="error">
-                       My error : {error}
-                    </div>
-                
-                    <button className="SignUp-Button">SignUp</button>
+                <input value={name} placeholder="Name" name="name" id="name" onChange={onchangeInputHandle} />
 
-                    <div className="already-account">
-                         Already have an account? <span>Login</span>
-                    </div>
+                <input value={email} placeholder="Email" name="email" id="email" onChange={onchangeInputHandle} />
 
-                    <div className="login-with-Google">Login with Google</div>
+                <input value={mobile} placeholder="Mobile No." name="mobile" id="number" onChange={onchangeInputHandle} />
+
+                <input value={createPass} placeholder="Create Password" name="createPass" id="password" onChange={onchangeInputHandle} />
+
+                <input value={confirmPass} placeholder="Confirm Password" name="confirmPass" id="confirm-password" onChange={onchangeInputHandle} />
+
+                <div className="error">
+                    {error}
+                </div>
+
+                <button className="SignUp-Button">SignUp</button>
+
+                <div className="already-account">
+                    Already have an account? <span>Login</span>
+                </div>
+
+                <div className="login-with-Google">Login with Google</div>
             </div>
         </form>
-        
+
     )
 }
 export default SignUp;
