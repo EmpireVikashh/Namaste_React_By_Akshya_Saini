2f5a6b66ccf1c5dd2dff319bb6dc6fc8131ed9ce
Vikash sharma <116377338+EmpireVikashh@users.noreply.github.com>
2023-09-13 12:03:29 +0000

    image part

--- SwiggyClone/src/MyPage/Body/Body.js
+++ SwiggyClone/src/MyPage/Body/Body.js
@@ -1,20 +1,32 @@
 import ShimmerEffect from '../ShimmerEffect/ShimmerEffect.js';
 import Header from '../Header/Header.js'
 import Footer from '../Footer/Footer.js'
+import jsonData from '../../SwiggyJsonData/myJson.json'
 import {useState, useEffect} from 'react';
+
+// *[@id="seo-core-layout"]/div[7]/div/div[1]/div[2]/a/div/div[1]/div/div/img
 
 const Body = ()=>{
     const [data, setData] = useState();
     useEffect(()=>{
+        
         getRestrauntData();
     }, [])
 
-    async function getRestrauntData(){
-        let prom = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8467126&lng=80.9460872&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
-        let resp = await prom.json();
-        // console.log(resp)
-        setData(resp);
-    }
+    // async function getRestrauntData(){
+    //     // let prom = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8467126&lng=80.9460872&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+    //     // let resp = await prom.json();
+    //     // console.log(resp)
+    //     // setData(resp);
+    // }
+
+      async function getRestrauntData() {
+          const mydata = setTimeout(()=>{setData(jsonData)},1000) ; // Parse JSON response
+          console.log(mydata)
+        //   setData(data);
+      
+      }
+
     console.log(data);
 
     return !data?<ShimmerEffect/>:(
