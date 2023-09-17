2170a709b80b6ad8c549dff10bd5105e31ed3b44
Vikash <empirevikash@gmail.com>
2023-09-17 11:59:05 +0530

    route

--- SwiggyClone/src/MyPage/Footer/Footer.js
+++ SwiggyClone/src/MyPage/Footer/Footer.js
@@ -2,6 +2,26 @@ const Footer = () => {
     return(
         <div id="footer">
             <h2>Footers</h2>
+            <h2>Footers</h2>
+            <h2>Footers</h2>
+            <h2>Footers</h2>
+            <h2>Footers</h2>
+            <h2>Footers</h2>
+            <h2>Footers</h2>
+            <h2>Footers</h2>
+            <h2>Footers</h2>
+            <h2>Footers</h2>
+            <h2>Footers</h2>
+            <h2>Footers</h2>
+            <h2>Footers</h2>
+            <h2>Footers</h2>
+            <h2>Footers</h2>
+            <h2>Footers</h2>
+            <h2>Footers</h2>
+            <h2>Footers</h2>
+            <h2>Footers</h2>
+            <h2>Footers</h2>
+            <h2>Footers</h2>
         </div>
     )
 }
