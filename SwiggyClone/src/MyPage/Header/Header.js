714a912fa253020eeb4bc93ae647dc2a40ebc1e7
Vikash <empirevikash@gmail.com>
2023-09-28 09:51:21 +0530

    filterPending

--- SwiggyClone/src/MyPage/Header/Header.js
+++ SwiggyClone/src/MyPage/Header/Header.js
@@ -1,5 +1,5 @@
 import UserLocation from "../Helper/UserLocation.js";
-import {SearchText} from "../Helper/SearchText.js";
+import SearchText from "../Helper/SearchText.js";
 import { Link } from "react-router-dom";
 import search from "../ImageFolder/search.png";
 import { useState } from "react";
--- SwiggyClone/src/MyPage/Helper/SearchText.js
+++ SwiggyClone/src/MyPage/Helper/SearchText.js
@@ -1,11 +1,41 @@
+// import RestaurantData from "../../SwiggyJsonData/myJson.json"
+// import { useState,useEffect } from "react"
 
-export const SearchText = (searchText)=>{
-    console.log(searchText)
-    return searchText;
-}
+// const SearchText = (searchText)=>{
 
-export const filterData = ()=>{
-    console.log(SearchText)
-    return "searchText";
-}
+//     const [resData,setResData] = useState(RestaurantData.card.topRestaurants.restaurants);
+   
+//         const data = RestaurantData.card.topRestaurants.restaurants;
+//         setResData(data);
+//         console.log(resData);
+//   console.log(searchText)
+
+// }
+
+// export default SearchText;
+
+import RestaurantData from "../../SwiggyJsonData/myJson.json";
+import { useState, useEffect } from "react";
+
+const SearchText = (searchText) => {
+  // Define resData state and initialize it with an initial value (an empty array in this case)
+  const [resData, setResData] = useState([]);
+
+  useEffect(() => {
+    // Fetch and set data when the component mounts
+    const data = RestaurantData.card.topRestaurants.restaurants;
+    setResData(data);
+  }, []); // Empty dependency array ensures this effect runs only once when the component mounts
+
+  // Logging here will not show the updated state immediately
+  console.log(resData);
+
+  return (
+    <div>
+      {/* Render your UI here */}
+    </div>
+  );
+};
+
+export default SearchText;
 
